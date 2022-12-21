e7d0b4ae969cc912246f6965cf3830b1d6cde08b
Jonathan Hernandez <pgr.gni.20@gmail.com>
2022-12-20 21:31:47 -0600

    Fusion of the menu on mobile

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 // Deskop menu
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burguerMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu')
 
+//deskttop Menu
 navEmail.addEventListener('click', toggleDeskMenu);
-
 function toggleDeskMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+// Burguer Menu Mobile
+burguerMenu.addEventListener('click', togglemobMenu);
+function togglemobMenu () {
+    mobileMenu.classList.toggle('inactive');
 }
